01885a43b7d2b28fda35cdae988b083446f50e4e
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-09-19 10:04:23 +0300

    Created a simple server and added simple routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 ////////////////////////?
 ////////?FILES
@@ -27,3 +29,26 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   });
 });
 console.log("Will read file!");
+
+//////////////?
+//////?SERVER
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page was not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
