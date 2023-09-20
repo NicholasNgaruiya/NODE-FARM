aafee3d26399d597f0fc74dbaa8d58213baec0a0
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-09-20 15:15:56 +0300

    replaced the overview  template with data from json

--- index.js
+++ index.js
@@ -33,19 +33,48 @@ console.log("Will read file!");
 //////////////?
 //////?SERVER
 //using blocking so that the code can be read only once at the beginning instead of everytime as is the case of non-blocking
+
+const replaceTemplate = (temp,product) =>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
   const pathName = req.url;
+
+  //overview
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "content-type": "text/html" });
+    //using map() to iterate
+    const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%TEMPLATE-CARD%}',cardHtml);
+
+    res.end(output);
+
+    //product
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    //api
   } else if (pathName === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
+
+    //not found
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
