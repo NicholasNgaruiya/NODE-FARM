1edb345ebc5d60cb7ee3866fb4c0b5b30720455d
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-09-20 16:08:34 +0300

    created my own module for the  replaceTemplate function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const replaceTemplate = require('./modules/replaceTemplate');
 /*
 ////////////////////////?
 ////////?FILES
@@ -34,18 +36,7 @@ console.log("Will read file!");
 //////?SERVER
 //using blocking so that the code can be read only once at the beginning instead of everytime as is the case of non-blocking
 
-const replaceTemplate = (temp,product) =>{
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-}
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");
