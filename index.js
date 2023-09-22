990fd1f36fd237b47d6c6531a07d017f2cd8007d
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-09-22 09:46:07 +0300

    added a prettierrc configuration for this project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 const slugify = require('slugify');
 
@@ -38,54 +38,52 @@ console.log("Will read file!");
 //////?SERVER
 //using blocking so that the code can be read only once at the beginning instead of everytime as is the case of non-blocking
 
-
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8");
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,"utf-8");
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
-
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
   // console.log(url.parse(req.url,true));
   //?using destructuring assignment
-  const { query, pathname } = url.parse(req.url,true);
+  const { query, pathname } = url.parse(req.url, true);
 
   //overview
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'content-type': 'text/html' });
     //using map() to iterate
-    const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-    const output = tempOverview.replace('{%TEMPLATE-CARD%}',cardHtml);
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%TEMPLATE-CARD%}', cardHtml);
 
     res.end(output);
 
     //product
-  } else if (pathname === "/product") {
+  } else if (pathname === '/product') {
     res.writeHead(200, { 'content-type': 'text/html' });
     const product = dataObj[query.id];
-    output = replaceTemplate(tempProduct,product);
+    output = replaceTemplate(tempProduct, product);
     res.end(output);
 
     //api
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "content-type": "application/json" });
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'content-type': 'application/json' });
     res.end(data);
 
     //not found
   } else {
     res.writeHead(404, {
-      "content-type": "text/html",
-      "my-own-header": "hello-world",
+      'content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-    res.end("<h1>Page was not found!</h1>");
+    res.end('<h1>Page was not found!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
